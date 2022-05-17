c53016268b6156e17eaacadaecc2bf32bb3e23ba
Valerii Kuzivanov <14344014+Valeronlol@users.noreply.github.com>
2022-05-17 21:09:23 +0300

    Release/1.4.0 (#12)
    Codestyle fixes

--- index.js
+++ index.js
@@ -10,24 +10,24 @@ const {
   isFunction,
   validateConfig,
   setConfig,
-  executeHook
-} = require('./lib/helpers');
-const {INTERNAL_ERROR} = require('./lib/error-codes');
+  executeHook,
+} = require('./lib/helpers')
+const { INTERNAL_ERROR, } = require('./lib/error-codes')
 
-const VERSION = '2.0';
+const VERSION = '2.0'
 
 /**
  *
  * @param {object} userConfig Custom user router configuration
  * @return {function} Express middleware
  */
-module.exports = userConfig => {
+module.exports = (userConfig) => {
   const config = {
     methods: {},
     beforeMethods: {},
     afterMethods: {},
-    onError: null
-  };
+    onError: null,
+  }
 
   /**
    * JSON RPC request handler
@@ -35,28 +35,37 @@ module.exports = userConfig => {
    * @return {Promise}
    */
   async function handleSingleReq(body, raw) {
-    const {id, method, jsonrpc, params = {}} = body;
+    const {
+      id, method, jsonrpc, params = {},
+    } = body
     try {
-      validateJsonRpcVersion(jsonrpc, VERSION);
+      validateJsonRpcVersion(jsonrpc, VERSION)
 
-      validateJsonRpcMethod(method, config.methods);
+      validateJsonRpcMethod(method, config.methods)
 
-      if (beforeMethod = (config.beforeMethods[method])) await executeHook(beforeMethod, params, null, raw);
+      const beforeMethod = config.beforeMethods[method]
+      if (beforeMethod) {
+        await executeHook(beforeMethod, params, null, raw)
+      }
 
-      const result = await config.methods[method](params, raw);
+      const result = await config.methods[method](params, raw)
 
-      if (afterMethod = (config.afterMethods[method])) await executeHook(afterMethod, params, result, raw);
+      const afterMethod = config.afterMethods[method]
+      if (afterMethod) {
+        await executeHook(afterMethod, params, result, raw)
+      }
 
-      if (!isNil(id)) return {jsonrpc, result, id}
+      if (!isNil(id)) return { jsonrpc, result, id, }
     } catch (err) {
-      if (isFunction(config.onError)) config.onError(err, body);
+      if (isFunction(config.onError)) config.onError(err, body)
       const error = {
         code: Number(err.code || err.status || INTERNAL_ERROR.code),
         message: err.message || INTERNAL_ERROR.message,
-      };
-      if (err && err.data) error.data = err.data;
-      return {jsonrpc, error, id: id || null}
+      }
+      if (err && err.data) error.data = err.data
+      return { jsonrpc, error, id: id || null, }
     }
+    return null
   }
 
   /**
@@ -67,25 +76,25 @@ module.exports = userConfig => {
   function handleBatchReq(bachBody, raw) {
     return Promise.all(
       bachBody.reduce((memo, body) => {
-        const result = handleSingleReq(body, raw);
-        if (!isNil(body.id)) memo.push(result);
+        const result = handleSingleReq(body, raw)
+        if (!isNil(body.id)) memo.push(result)
         return memo
       }, [])
-    );
+    )
   }
 
-  validateConfig(userConfig);
+  validateConfig(userConfig)
 
-  setConfig(config, userConfig);
+  setConfig(config, userConfig)
 
   return async (req, res, next) => {
-    const rpcData = req.body;
+    const rpcData = req.body
     if (Array.isArray(rpcData)) {
-      res.send(await handleBatchReq(rpcData, { req, res }))
+      res.send(await handleBatchReq(rpcData, { req, res, }))
     } else if (typeof rpcData === 'object') {
-      res.send(await handleSingleReq(rpcData, { req, res }))
+      res.send(await handleSingleReq(rpcData, { req, res, }))
     } else {
       next(new Error('JSON-RPC router error: req.body is required. Ensure that you install body-parser and apply it before json-router.'))
     }
   }
-};
+}
